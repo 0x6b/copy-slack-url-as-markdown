b81de8503faf42b29db6436990d55b7a28ed0487
kaoru <k@warpnine.io>
2024-08-28 12:12:28 +0900

    refactor: organize modules

--- src/slack_client/mod.rs
+++ src/slack_client/mod.rs
@@ -1,5 +1,7 @@
 mod client;
 pub mod query;
 pub mod response;
+pub mod message;
 
 pub use client::Client;
+pub use message::SlackMessage;
---
+++ src/template/mod.rs
@@ -0,0 +1,5 @@
+pub mod context_key;
+pub mod template_type;
+
+pub use context_key::ContextKey;
+pub use template_type::TemplateType;
