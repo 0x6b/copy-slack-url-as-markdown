4a062f0ef0e7665799d132ad2fade6d0fe6b8a52
kaoru <k@warpnine.io>
2024-08-28 12:07:03 +0900

    refactor: optimize import

--- src/slack_client/client.rs
+++ src/slack_client/client.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
 use serde_qs::to_string;
 
@@ -13,7 +14,7 @@ pub struct Client {
 
 impl Client {
     /// Create a new Slack API client.
-    pub fn new(token: &str) -> anyhow::Result<Self> {
+    pub fn new(token: &str) -> Result<Self> {
         let client = reqwest::Client::builder()
             .default_headers(HeaderMap::from_iter([
                 (CONTENT_TYPE, HeaderValue::from_static("application/json")),
@@ -24,28 +25,22 @@ impl Client {
     }
 
     // https://api.slack.com/methods/conversations.info
-    pub async fn conversations_info(&self, query: &Info<'_>) -> anyhow::Result<Channel> {
+    pub async fn conversations_info(&self, query: &Info<'_>) -> Result<Channel> {
         Ok(self.request::<Info>(query).await?.channel)
     }
 
     // https://api.slack.com/methods/conversations.history
-    pub async fn conversations_history(
-        &self,
-        query: &History<'_>,
-    ) -> anyhow::Result<Option<Vec<Message>>> {
+    pub async fn conversations_history(&self, query: &History<'_>) -> Result<Option<Vec<Message>>> {
         Ok(self.request::<History>(query).await?.messages)
     }
 
     // https://api.slack.com/methods/conversations.replies
-    pub async fn conversations_replies(
-        &self,
-        query: &Replies<'_>,
-    ) -> anyhow::Result<Option<Vec<Message>>> {
+    pub async fn conversations_replies(&self, query: &Replies<'_>) -> Result<Option<Vec<Message>>> {
         Ok(self.request::<Replies>(query).await?.messages)
     }
 
     // Helper method to make a request and deserialize the response into `T`
-    async fn request<T>(&self, query: &T) -> anyhow::Result<T::Response>
+    async fn request<T>(&self, query: &T) -> Result<T::Response>
     where
         T: Query,
     {
