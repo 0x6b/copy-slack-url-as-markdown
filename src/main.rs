ebfb06a89c7b4716f51f46a2fcfc57e5a6aa4c65
kaoru <k@warpnine.io>
2024-08-29 12:18:42 +0900

    chore: more friendly error messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::{bail, Result};
+
 use crate::copier::Copier;
 
 mod copier;
@@ -5,15 +7,24 @@ mod state;
 mod template;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
     let copier = Copier::new().await?;
 
     let mut clipboard = arboard::Clipboard::new()?;
-    let copier = copier
-        .resolve(&url::Url::parse(clipboard.get_text()?.trim())?)
-        .await?;
 
-    let (rich_text, text) = copier.render()?;
+    let text = match clipboard.get_text() {
+        Ok(text) => text,
+        Err(_) => bail!("failed to access system clipboard"),
+    };
+
+    let url = match url::Url::parse(text.trim()) {
+        Ok(url) => url,
+        Err(_) => bail!("invalid URL provided: {text}"),
+    };
+
+    let message = copier.resolve(&url).await?;
+
+    let (rich_text, text) = message.render()?;
     match clipboard.set_html(rich_text.trim(), Some(text.trim())) {
         Ok(_) => println!("{text}"),
         Err(why) => println!("{why}"),
