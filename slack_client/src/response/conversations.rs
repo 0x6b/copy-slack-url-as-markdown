2fa75752c9a54f8868c84ff10a072329450174ff
kaoru <k@warpnine.io>
2024-08-30 23:03:09 +0900

    feat: use blocks as the source of the text by default
    A naive and ugly mrkdwn to markdown converter, which could be an independent crate in the future.

--- slack_client/src/response/conversations.rs
+++ slack_client/src/response/conversations.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::Deserialize;
 
 use crate::response::Response;
@@ -39,9 +41,222 @@ pub struct Message {
     pub user: String,
     /// The text of the message.
     pub text: Option<String>,
+    /// The Slack block kit blocks of the message.
+    pub blocks: Option<Vec<Block>>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Purpose {
     pub value: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Block {
+    #[serde(rename = "type")]
+    pub block_type: BlockType,
+    pub elements: Option<Vec<RichTextElement>>,
+}
+
+impl Display for Block {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.elements {
+            Some(elements) => {
+                for element in elements {
+                    write!(f, "{}", element)?;
+                }
+            }
+            None => {}
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum BlockType {
+    RichText,
+    Header,
+    Divider,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum RichTextElement {
+    RichTextSection {
+        elements: Vec<RichTextElement>,
+    },
+    RichTextQuote {
+        elements: Vec<RichTextElement>,
+    },
+    RichTextList {
+        style: ListStyle,
+        indent: i64,
+        elements: Vec<RichTextElement>,
+    },
+    RichTextPreformatted {
+        elements: Vec<RichTextElement>,
+    },
+    Emoji {
+        name: String,
+        style: Option<Style>,
+    },
+    Text {
+        text: String,
+        style: Option<Style>,
+    },
+    Link {
+        url: String,
+        text: Option<String>,
+    },
+    User {
+        user_id: String,
+    },
+    #[serde(rename = "usergroup")]
+    UserGroup {
+        usergroup_id: String,
+    },
+    Broadcast {
+        range: String,
+    },
+    Channel {
+        channel_id: String,
+        style: Option<Style>,
+    },
+}
+
+impl Display for RichTextElement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            RichTextElement::RichTextSection { elements } => {
+                let mut result = String::new();
+                for element in elements {
+                    result.push_str(&element.to_string());
+                }
+                result
+            }
+            RichTextElement::RichTextQuote { elements } => {
+                let mut result = String::new();
+                for element in elements {
+                    result.push_str(&format!("> {}", element));
+                }
+                result
+            }
+            RichTextElement::RichTextList { style, indent, elements } => {
+                let mut result = String::new();
+                for (n, element) in elements.iter().enumerate() {
+                    match style {
+                        ListStyle::Bullet => {
+                            result.push_str(&"  ".repeat(*indent as usize));
+                            result.push_str("- ");
+                        }
+                        ListStyle::Ordered => {
+                            result.push_str(&"   ".repeat(*indent as usize));
+                            result.push_str(&format!("{}. ", n + 1));
+                        }
+                    }
+                    result.push_str(&element.to_string());
+                    result.push('\n');
+                }
+                result
+            }
+            RichTextElement::RichTextPreformatted { elements } => {
+                let mut result = String::new();
+                for element in elements {
+                    result.push_str("```\n");
+                    result.push_str(&element.to_string());
+                    result.push_str("\n```\n");
+                }
+                result
+            }
+            RichTextElement::Emoji { name, style: _style } => {
+                let mut result = String::new();
+                result.push_str(name);
+                result
+            }
+            RichTextElement::Text { text, style } => {
+                let mut result = String::new();
+                match style {
+                    Some(Style { code, bold, italic, strike }) => {
+                        let (code, bold, italic, strike) = (
+                            code.unwrap_or_default(),
+                            bold.unwrap_or_default(),
+                            italic.unwrap_or_default(),
+                            strike.unwrap_or_default(),
+                        );
+                        result.push_str(
+                            &Some(text.to_string())
+                                .map(|t| if code { format!("`{}`", t) } else { t })
+                                .map(|t| if bold { format!("**{}**", t) } else { t })
+                                .map(|t| if italic { format!("_{}_", t) } else { t })
+                                .map(|t| if strike { format!("~~{}~~", t) } else { t })
+                                .unwrap(),
+                        );
+                    }
+                    None => {
+                        result.push_str(text);
+                    }
+                }
+                result
+            }
+            RichTextElement::Link { url, text } => {
+                let mut result = String::new();
+                match text {
+                    Some(t) => {
+                        result.push('[');
+                        result.push_str(t);
+                        result.push_str("](");
+                        result.push_str(url);
+                        result.push(')');
+                    }
+                    None => {
+                        result.push_str(url);
+                    }
+                }
+                result
+            }
+            RichTextElement::User { user_id } => {
+                let mut result = String::new();
+                result.push_str("<@");
+                result.push_str(user_id);
+                result.push('>');
+                result
+            }
+            RichTextElement::UserGroup { usergroup_id } => {
+                let mut result = String::new();
+                result.push_str("<!subteam^");
+                result.push_str(usergroup_id);
+                result.push('>');
+                result
+            }
+            RichTextElement::Broadcast { range } => {
+                let mut result = String::new();
+                result.push('@');
+                result.push_str(range);
+                result
+            }
+            RichTextElement::Channel { channel_id, style: _style } => {
+                let mut result = String::new();
+                result.push_str("<#");
+                result.push_str(channel_id);
+                result.push('>');
+                result
+            }
+        };
+        write!(f, "{}", str)
+    }
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Style {
+    code: Option<bool>,
+    bold: Option<bool>,
+    italic: Option<bool>,
+    strike: Option<bool>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "lowercase")]
+pub enum ListStyle {
+    Bullet,
+    Ordered,
 }
