9bcf4353351e7150d6b52df1eaf7ca0cd1b0bc63
kaoru <k@warpnine.io>
2024-08-31 17:21:01 +0900

    refactor: organize modules

--- slack_client/src/message/mod.rs
+++ slack_client/src/message/mod.rs
@@ -1,3 +1,5 @@
+pub mod state;
+
 use std::{
     ops::{Deref, DerefMut},
     sync::LazyLock,
@@ -9,6 +11,7 @@ use serde::Deserialize;
 use url::Url;
 
 use crate::{
+    message::state::{Initialized, Resolved, State},
     request::{
         bots::Info as BotsInfo,
         conversations::{History, Info as ConversationsInfo, Replies},
@@ -17,7 +20,6 @@ use crate::{
     },
     response::{
         conversations::{Message, Purpose},
-        usergroups::Usergroup,
         users::User,
     },
     Client, Emojify,
@@ -34,37 +36,6 @@ static RE_LINK: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"<([^|]+)\|([^>]+
 struct QueryParams {
     thread_ts: Option<f64>,
 }
-
-pub trait State {}
-#[derive(Debug)]
-pub struct Initialized<'a> {
-    /// The plain URL.
-    pub url: &'a Url,
-    /// The channel ID.
-    pub channel_id: &'a str,
-    /// The timestamp as &str.
-    pub ts: &'a str,
-    /// The timestamp as f64.
-    pub ts64: f64,
-    /// The thread timestamp as f64.
-    pub thread_ts64: Option<f64>,
-    /// Cache the usergroups to avoid fetching it multiple times, as there is no API to fetch a
-    /// single usergroup.
-    usergroups: Option<Vec<Usergroup>>,
-}
-
-impl<'a> State for Initialized<'a> {}
-
-#[derive(Debug)]
-pub struct Resolved<'a> {
-    pub url: &'a Url,
-    pub channel_name: String,
-    pub user_name: String,
-    pub body: String,
-    pub ts: i64,
-}
-
-impl<'a> State for Resolved<'a> {}
 
 #[derive(Debug)]
 pub struct SlackMessage<S>
