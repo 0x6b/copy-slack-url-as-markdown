30c1e9b86d597664f3c8b08e7b5497fec14203db
kaoru <k@warpnine.io>
2024-08-31 17:35:27 +0900

    docs: add code comments

--- slack_client/src/message/state.rs
+++ slack_client/src/message/state.rs
@@ -2,7 +2,19 @@ use url::Url;
 
 use crate::response::usergroups::Usergroup;
 
+/// A marker trait for the state of a Slack message.
+///
+/// This trait is used to ensure that the message is in a valid state before performing any
+/// operations.
+///
+/// Possible states are:
+///
+/// - `Initialized`: The message has been initialized with the URL, channel ID, and timestamp.
+/// - `Resolved`: The message has been resolved with the channel name, user name, and message body.
 pub trait State {}
+impl<'a> State for Initialized<'a> {}
+impl<'a> State for Resolved<'a> {}
+
 #[derive(Debug)]
 pub struct Initialized<'a> {
     /// The plain URL.
@@ -20,15 +32,16 @@ pub struct Initialized<'a> {
     pub(crate) usergroups: Option<Vec<Usergroup>>,
 }
 
-impl<'a> State for Initialized<'a> {}
-
 #[derive(Debug)]
 pub struct Resolved<'a> {
+    /// The plain URL.
     pub url: &'a Url,
+    /// The channel name.
     pub channel_name: String,
+    /// The user name.
     pub user_name: String,
+    /// The message body.
     pub body: String,
+    /// The timestamp as f64.
     pub ts: i64,
 }
-
-impl<'a> State for Resolved<'a> {}
